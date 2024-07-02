9f8da86ddc62ca706d5e4d25a2cb58fe80bcea6b
Genesis Lovino <genesislovino@gmail.com>
2024-07-02 13:58:54 +0800

    Created models and migrations

--- fouruni/settings.py
+++ fouruni/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -126,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'main.User'
--- main/models.py
+++ main/models.py
@@ -23,7 +23,7 @@ class University(models.Model):
 class Board(models.Model):
     board_id = models.CharField(max_length=5, primary_key=True)
     name = models.CharField(max_length=50)
-    description = models.CharField(default="A board.") # Can be a motto of the university or a simple description of the board.
+    description = models.CharField(default="A board.", max_length=100, blank=True) # Can be a motto of the university or a simple description of the board.
 
     # TODO: Add sorting using class Meta
 
