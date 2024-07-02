082a89617f5c929389ea30b459de77c34c6713a7
Genesis Lovino <genesislovino@gmail.com>
2024-07-02 15:39:19 +0800

    test: Added Board test

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from main.models import Board
 
 # Create your tests here.
+class BoardTest(TestCase):
+    def setUp(self):
+        Board.objects.create(board_id="pup", name="Polytecnic Universitiy of The Philippines")
+
+    def test_get_board(self):
+        board = Board.objects.get(board_id='pup')
+        self.assertEqual(board.board_id, 'pup')
