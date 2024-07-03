cb8dc7afe18ab2a4a7dcb3e643d8aad6fe903a26
Genesis Lovino <genesislovino@gmail.com>
2024-07-03 08:05:14 +0800

    refactor: Change redirections

--- main/views.py
+++ main/views.py
@@ -92,7 +92,7 @@ def create_thread(request, board_id):
             thread.save()
 
             # TODO: Redirect to the thread itself
-            return redirect('main:threads', board_id=board_id)
+            return redirect('main:post', board_id=board_id, thread_id=thread.id)
         
 
 def post(request, board_id, thread_id):
@@ -109,6 +109,7 @@ def post(request, board_id, thread_id):
             'replies': replies
         }
         return render(request, "base.html", context)
+    
     elif request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
